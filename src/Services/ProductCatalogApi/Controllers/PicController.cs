5cfa1946b8eb5a9819ae769895cccffc026b9fde
Simon Bauer <simon.bauer@software-wizard.de>
2018-02-07 14:18:18 +0100

    Pic controller added

---
+++ src/Services/ProductCatalogApi/Controllers/PicController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ProductCatalogApi.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    public class PicController : Controller
+    {
+        private readonly IHostingEnvironment _env;
+
+        public PicController(IHostingEnvironment env)
+        {
+            _env = env;
+        }
+        [HttpGet]
+        [Route("{id}")]
+        public IActionResult GetImage(int id)
+        {
+            var webRoot = _env.WebRootPath;
+            var path = Path.Combine($"{webRoot}/Pics/", $"shoes-{id}.png");
+
+            try
+            {
+                var buffer = System.IO.File.ReadAllBytes(path);
+                return File(buffer, "image/png");
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception);
+                return NotFound();
+            }
+        }
+    }
+}
