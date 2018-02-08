45167a8c9bf7ed3cd1646d1f64599d1108b1ebd8
Simon Bauer <simon.bauer@software-wizard.de>
2018-02-08 13:20:47 +0100

    bugfix PictureUrl

--- src/Web/WebMvc/Models/CatalogItem.cs
+++ src/Web/WebMvc/Models/CatalogItem.cs
@@ -6,7 +6,7 @@
         public string Name { get; set; }
         public string Description { get; set; }
         public decimal Price { get; set; }
-        public string PictureUri { get; set; }
+        public string PictureUrl { get; set; }
         public int CatalogBrandId { get; set; }
         public string CatalogBrand { get; set; }
         public int CatalogTypeId { get; set; }
