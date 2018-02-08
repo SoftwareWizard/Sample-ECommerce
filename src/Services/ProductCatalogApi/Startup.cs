f48ed4d18513a31a1e323bcd59bc3abb590766e7
Simon Bauer <simon.bauer@software-wizard.de>
2018-02-08 09:34:56 +0100

    Startup refactored

--- src/Services/ProductCatalogApi/Startup.cs
+++ src/Services/ProductCatalogApi/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using ProductCatalogApi.Data;
+using Swashbuckle.AspNetCore.SwaggerGen;
 
 namespace ProductCatalogApi
 {
@@ -20,32 +21,32 @@ namespace ProductCatalogApi
             Configuration = configuration;
         }
 
+        private string ConnectionString
+        {
+            get
+            {
+                var server = Configuration["DatabaseServer"];
+                var database = Configuration["DatabaseName"];
+                var user = Configuration["DatabaseUser"];
+                var password = Configuration["DatabaseUserPassword"];
+                var connectionString = $"Server={server};" +
+                                       $"Database={database};" +
+                                       $"User Id={user};" +
+                                       $"Password={password};" +
+                                       "MultipleActiveResultSets=true";
+                return connectionString;
+            }
+        }
+
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.Configure<CatalogSettings>(Configuration);
-
-            var server = Configuration["DatabaseServer"];
-            var database = Configuration["DatabaseName"];
-            var user = Configuration["DatabaseUser"];
-            var password = Configuration["DatabaseUserPassword"];
-            var connectionString = $"Server={server};Database={database};User Id={user};Password={password};MultipleActiveResultSets=true";
-            services.AddDbContext<CatalogContext>(builder => builder.UseSqlServer(connectionString));
+            services.AddDbContext<CatalogContext>(builder => builder.UseSqlServer(ConnectionString));
             services.AddMvc();
-
-            services.AddSwaggerGen(options =>
-            {
-                options.DescribeAllEnumsAsStrings();
-                options.SwaggerDoc("v1", new Swashbuckle.AspNetCore.Swagger.Info
-                {
-                    Title = "ShoesOnContainers - Product Catalog HTTP API",
-                    Version = "v1",
-                    Description = "The Product Catalog Microservice HTTP API. This is a Data-Driven/CRUD microservice sample",
-                    TermsOfService = "Terms Of Service"
-                });
-            });
+            services.AddSwaggerGen(SwaggerConfiguration);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -65,5 +66,18 @@ namespace ProductCatalogApi
 
             app.UseMvc();
         }
+
+        private void SwaggerConfiguration(SwaggerGenOptions options)
+        {
+            options.DescribeAllEnumsAsStrings();
+            options.SwaggerDoc("v1", new Swashbuckle.AspNetCore.Swagger.Info
+            {
+                Title = "ShoesOnContainers - Product Catalog HTTP API",
+                Version = "v1",
+                Description = "The Product Catalog Microservice HTTP API. This is a Data-Driven/CRUD microservice sample",
+                TermsOfService = "Terms Of Service"
+            });
+        }
+
     }
 }
