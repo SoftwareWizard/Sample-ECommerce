1e28becde040d38bdebea8499f0d94afd2889e4d
Simon Bauer <simon.bauer@software-wizard.de>
2018-02-08 16:45:26 +0100

    Finish TokenServiceApi

--- src/Services/TokenServiceApi/Startup.cs
+++ src/Services/TokenServiceApi/Startup.cs
@@ -37,6 +37,15 @@ namespace TokenServiceApi
             services.AddTransient<IEmailSender, EmailSender>();
 
             services.AddMvc();
+
+            // configure identity server with in-memory stores, keys, clients and scopes
+            services.AddIdentityServer()
+                .AddDeveloperSigningCredential()
+                .AddInMemoryPersistedGrants()
+                .AddInMemoryIdentityResources(Config.GetIdentityResources())
+                .AddInMemoryApiResources(Config.GetApiResources())
+                .AddInMemoryClients(Config.GetClients(Config.GetUrls(Configuration)))
+                .AddAspNetIdentity<ApplicationUser>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -55,7 +64,8 @@ namespace TokenServiceApi
 
             app.UseStaticFiles();
 
-            app.UseAuthentication();
+            // app.UseIdentity(); // not needed, since UseIdentityServer adds the authentication middleware
+            app.UseIdentityServer();
 
             app.UseMvc(routes =>
             {
